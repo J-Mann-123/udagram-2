571c0a088f80f8341fb325e243b7bfd02ba342ef
J-Mann-123 <jmann5326@gmail.com>
2023-11-12 22:35:39 +1030

    fixed linting

--- udagram/udagram-frontend/src/app/feed/services/feed.provider.service.ts
+++ udagram/udagram-frontend/src/app/feed/services/feed.provider.service.ts
@@ -12,15 +12,15 @@ export class FeedProviderService {
 
   constructor(private api: ApiService) { }
 
-  async getFeed(): Promise<BehaviorSubject<FeedItem[]>> {
+  async getFeed (): Promise<BehaviorSubject<FeedItem[]>> {
     const req = await this.api.get('/feed');
-    const items = <FeedItem[]> req.rows;
+    const items = <FeedItem[]>req.rows;
     this.currentFeed$.next(items);
     return Promise.resolve(this.currentFeed$);
   }
 
-  async uploadFeedItem(caption: string, file: File): Promise<any> {
-    const res = await this.api.upload('/feed', file, {caption: caption, url: file.name});
+  async uploadFeedItem (caption: string, file: File): Promise<any> {
+    const res = await this.api.upload('/feed', file, { caption: caption, url: file.name });
     const feed = [res, ...this.currentFeed$.value];
     this.currentFeed$.next(feed);
     return res;
